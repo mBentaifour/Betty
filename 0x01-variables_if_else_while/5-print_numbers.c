30d8cfd2685eae6caa478de18d294ac6f4a974f8
mBentaifour <bentaifourmoh@gmail.com>
2023-05-19 15:17:34 -0700

    Update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the Numbers,
+ * followed by a new line
+ * description: 'print numbers of base'
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n; 
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	print("\n");
+	return (0);
+}
