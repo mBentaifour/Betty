e0388123dc716a44005fb6a95d83862a6498ce88
mBentaifour <bentaifourmoh@gmail.com>
2023-05-19 16:18:01 -0700

    Update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible combinations of single-digit numbers,
+ * main - program that prints all possible combinations
  * followed by a new line
  * description: 'print combination'
  * Return: always 0
