d991528be5011fb89866d5126123469ed81b450c
mBentaifour <bentaifourmoh@gmail.com>
2023-05-19 15:29:12 -0700

    Update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the Numbers,
+ * followed by a new line
+ * description: 'print numbers of base'
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar("\n");
+	return (0);
+}
