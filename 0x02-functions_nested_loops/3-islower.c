1cf9c0acf0520202186facc1e7e4ef4bb0a898d8
mBentaifour <bentaifourmoh@gmail.com>
2023-05-23 18:07:46 -0700

    Update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,15 @@
 /**
  * _islower - a function that checks for lowercase character,
  * Return: returns 1 if 'c' is lowercase
- *         Returns 0 otherwise
+ *         otherwise always 0 (Success)
  */
 
-int _islower(int c);
+int _islower(int c)
+
 {
-	if (c > 96 && c < 123)
-		return (1);
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
 	return (0);
 }
