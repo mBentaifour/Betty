cdcacd76fb87f52e3b56c92c52957dcb2c29cb5a
mBentaifour <bentaifourmoh@gmail.com>
2023-05-23 17:22:33 -0700

    Update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_islower - a function that checks for lowercase character,
+ *
+ */
+
+void functions_nested_loops(void)
+{
+	int c;
+	        if (c > 96 && c < 123)
+	        	return (1);
+		return (0)
+}
