38a8b214846830335812bcefeb2ab5f73832055f
mBentaifour <bentaifourmoh@gmail.com>
2023-05-23 22:16:21 -0700

    Update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,23 +2,47 @@
 
 /**
  * jack_bauer - prints minute of the day
+ * Return: void
  */
 
 void jack_bauer(void)
 {
-	int hr, min;
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+	int a = 9;
 
-	for (hr = 0; hr <= 23; hr++)
+	h2 = 0;
+	while (h2 <= 2)
 	{
-		for (min = 0; min <= 59; min++)
+		if (h2 == 2)
 		{
-			-putchar((hr / 10) + 48);
-			-putchar((hr % 10) + 48);
-			-putchar(':');
-			-putchar((min / 10) + 48);
-			-putchar((min % 10) + 48);
-			-putchar('\n');
+			/*Restrain to 23h, not 29*/
+			a = 3;
 		}
+		h1 = 0;
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					-putchar('0' + h2);
+					-putchar('0' + h1);
+					-putchar(':');
+					-putchar('0' + m2);
+					-putchar('0' + m1);
+					-putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
 	}
 }
 
