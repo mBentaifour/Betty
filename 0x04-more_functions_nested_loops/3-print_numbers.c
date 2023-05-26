f3ef4c3340bfaf9030a8ab267720cacf7a1f3769
mBentaifour <bentaifourmoh@gmail.com>
2023-05-26 11:55:41 -0700

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,17 +4,17 @@
  * print_numbers - print 0 - 9
  * only using _putchar twice
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int num = 0
+	char c;
 
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
 
