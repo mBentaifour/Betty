2434684d1bde56bfd49747ad6e697c07994a5d40
mBentaifour <bentaifourmoh@gmail.com>
2023-06-01 21:12:09 -0700

    Update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,6 +16,8 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+void printASCII(char *b, int start, int end);
+void printHexes(char *b, int start, int end);
+int isPrintableASCII(int n);
 
 #endif
