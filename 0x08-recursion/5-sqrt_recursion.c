322f0aad9b14dd50aa34b17930d740dff5e3174a
mBentaifour <bentaifourmoh@gmail.com>
2023-06-08 09:38:05 -0700

    Update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int sqrt_alt(int n,int i);
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: int
+ * Return: If n does not have a natural square root,
+ * the function should return -1
+ */
+
+
+int _sqrt_recursion(int n)
+
+{
+	return (sqrt_alt(n, 0));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: int
+ * @imp: square root
+ * Return: If n does not have a natural square root,
+ * the function should return -1
+ */
+int sqrt_alt(int n,int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i > n)
+		return (-1);
+	if (i < 0)
+		return (-1);
+	return (sqrt_alt(n ,i + 1));
+}
