4e2ba3888ab76022a368266c47d5c44651db9985
mBentaifour <bentaifourmoh@gmail.com>
2023-06-10 11:10:45 -0700

    Update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the number 
+ * main - a program that prints the number
  * of arguments passed into it.
  * @argc: number of commandline
  * @argv: pointer to commandline
- * Return: 0 if right, non zero fail 
+ * Return: 0 if right, non zero fail
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
