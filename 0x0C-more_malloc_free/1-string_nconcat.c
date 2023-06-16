22797f56236def6f56154a2ecef9fc5a18e9329c
mBentaifour <bentaifourmoh@gmail.com>
2023-06-15 17:49:19 -0700

    Update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 /**
  * *string_nconcat - a function that concatenates two strings.
  * If the function fails, it should return NULL
- * n is greater or equal to the length 
+ * n is greater or equal to the length
  * of s2 then use the entire string s2
  *
  * @s1: first pointer to string.
@@ -28,5 +28,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 		;
+	/*memory reservation for case 1 & 2.*/
+	str = malloc(s1_length + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
 
+	str[i] = '\0';
+	return (str);
 }
