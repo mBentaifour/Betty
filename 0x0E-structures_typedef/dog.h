37f4fe11eef33bed981ee22218539763d0b13abb
mBentaifour <bentaifourmoh@gmail.com>
2023-06-19 18:48:23 -0700

    Update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * Define a new type struct dog with the following elements
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
