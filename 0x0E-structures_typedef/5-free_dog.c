6154d907dd91b3f93b88a6f3ce32ed9abc101c54
mBentaifour <bentaifourmoh@gmail.com>
2023-06-20 07:52:19 -0700

    Update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: free mydog
+ *
+ * Return: void.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
