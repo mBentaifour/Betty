68cbc1e5e52b937aa37fcff199d340e07cc67777
mBentaifour <bentaifourmoh@gmail.com>
2023-11-18 05:49:30 -0800

    master

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+
+	mov rdi,format
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+
+	mov rax,0
+	ret
+
+section .data
+	msg db "Hello, Holberton", 0
+	format db "%s", 10, 0
