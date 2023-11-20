d8d5b13d638b75b156bc59604883a264bb9c5e8c
mBentaifour <bentaifourmoh@gmail.com>
2023-11-20 15:57:50 -0800

    master

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,20 +5,20 @@
 * print_listint - a Prints elements listint_t list
 * @h: Pointer to the head of list_t list
 *
-* Return: number of killnod
+* Return: number of nodes
 */
 
 size_t print_listint(const listint_t *h)
 {
-        size_t killnod = 0;
+        size_t nodes = 0;
 
         if (h == NULL)
                 return (0);
         while (h != NULL)
         {
-          printf("%d\n", (*h).n);
-                h = (*h).next;
-                killnod++;
+          printf("%d\n", h->n);
+                h = h->next;
+                nodes++;
         }
-        return (killnod);
+        return (nodes);
 }
