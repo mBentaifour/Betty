2f1f380d60828aa0a65610a9212678be50b27b09
mBentaifour <bentaifourmoh@gmail.com>
2023-11-20 14:50:18 -0800

    list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len -  returns the numbers of elements
+ * @h: Pointer to the head of list_t list
+ *
+ * Return: number of listint_t list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+        size_t killnode = 0;
+
+        while (h)
+        {
+                killnode++;
+                h = (*h).next;
+        }
+
+        return (killnode);
+}
