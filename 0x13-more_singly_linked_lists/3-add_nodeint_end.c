94948da184f641f09a619ead01c5b413ed9f278a
mBentaifour <bentaifourmoh@gmail.com>
2023-11-21 14:00:29 -0800

    master

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,9 +25,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	else
 	{
-		last = *head
-			while ((*last).next != NULL)
-				last = (*last).next;
+		last = *head;
+		while ((*last).next != NULL)
+			last = (*last).next;
 		(*last).next = neo;
 	}
 
