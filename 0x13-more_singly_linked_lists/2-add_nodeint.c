9a7cbeae4424fb35159ab08e2a0cc360ad7e2fa4
mBentaifour <bentaifourmoh@gmail.com>
2023-11-20 16:31:03 -0800

    master

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* add_nodeint_end - a new node at the end
+* @head: A pointer to the address of the head
+* @n: Integer for the new node to contain
+*
+* Return: the new element, or NULL if it failed
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+        listint_t *neo, *last;
+
+        neo = malloc(sizeof(listint_t));
+        if (neo == NULL)
+                return (NULL);
+
+        (*neo).n = n;
+        (*neo).next = NULL;
+
+        if (*head == NULL)
+                *head = neo;
+
+        else
+        {
+                last = *head;
+		while ((*last).next != NULL)
+		      last = (*last).next;
+                (*last).next = neo;
+        }
+
+        return (*head);
+}
