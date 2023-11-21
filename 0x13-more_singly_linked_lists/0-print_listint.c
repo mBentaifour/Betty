bcf7af94c922c246e3490e1529a8cb13a5c16c09
mBentaifour <bentaifourmoh@gmail.com>
2023-11-21 13:24:30 -0800

    master

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,17 @@
 */
 
 size_t print_listint(const listint_t *h)
-{
-        size_t nodes = 0;
 
-        if (h == NULL)
-                return (0);
-        while (h != NULL)
-        {
-          printf("%d\n", h->n);
-                h = h->next;
-                nodes++;
-        }
-        return (nodes);
+{
+	size_t nod = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%d\n", (*h).n);
+		h = (*h).next;
+		nod++;
+	}
+	return (nod);
 }
