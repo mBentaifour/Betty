850b75d20af9eacc6f7b7c189490d01fa89b91bb
mBentaifour <bentaifourmoh@gmail.com>
2024-06-03 12:08:15 -0700

    Makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid
+Create a function def island_perimeter(grid)
+that returns the perimeter of the island described in grid
+
 """
 
 
